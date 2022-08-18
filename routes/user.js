4c60c0daeb232e0d2c547201ade0122054895ca8
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-08-18 21:40:30 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const mysql = require('mysql')
+const db = require('../configs/db.configs')
+
+const connection = mysql.createConnection(db.database)
+connection.connect(function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("connected to the mysql"); 
+    }
+})
 
 router.get('/', (req,res)=>{
     res.send('get user')
 })
 
 router.post('/', (req,res)=>{
+    console.log(req.body);
     res.send('post user')
 })
 router.put('/', (req,res)=>{
+    console.log(req.body);
     res.send('put user')
 })
 
 router.delete('/:id', (req,res)=>{
+    console.log(req.params.id);
     res.send('delete user')
 })
 
 router.get('/:id', (req,res)=>{
+    console.log(req.params.id);
     res.send('get user by id')
 })
 
