7a71c77e729e5eadfe8cab046cb8d750fdcfeaed
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-08-18 23:15:02 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -9,16 +9,39 @@ connection.connect(function(err){
         console.log(err);
     }else{
         console.log("connected to the mysql"); 
+        var userTableQuery = "CREATE TABLE IF NOT EXISTS users (id VARCHAR(255) PRIMARY KEY, name VARCHAR(255), username VARCHAR(255))"
+        connection.query(userTableQuery, function(err,result){
+            if(err) throw err;
+            if(result.warningCount === 0){
+                console.log("user table created");
+            }
+        })
     }
 })
 
 router.get('/', (req,res)=>{
-    res.send('get user')
+    var query = "SELECT * FROM users";
+    connection.query(query, (err, rows)=>{
+        if(err) throw err
+        res.send(rows)
+    })
 })
 
 router.post('/', (req,res)=>{
     console.log(req.body);
-    res.send('post user')
+    const id = req.body.id
+    const name = req.body.name
+    const username = req.body.username
+
+    var query = "INSERT INTO users (id, name, username) VALUES (?,?,?)";
+    connection.query(query,[id,name,username], (err)=>{
+        if(err){
+            res.send({'message' : 'duplicate entry'})
+        }else{
+            res.send({'message' : 'user created'})
+        }
+    })
+
 })
 router.put('/', (req,res)=>{
     console.log(req.body);
